7c8f47f5bd51c755c396ae6937d15de52bbca475
George Perdrizet <george@perdrizet.org>
2025-02-10 19:34:49 +0000

    Finished exercise 3.

--- exercises/02-random-status/app.py
+++ exercises/02-random-status/app.py
@@ -1,3 +1,16 @@
 import requests
 
 response = requests.get("https://assets.breatheco.de/apis/fake/sample/random-status.php")
+
+response_codes={
+    '404': 'The URL you asked for is not found',
+    '503': 'Unavailable right now',
+    '200': 'Everything went perfect',
+    '400': 'Something is wrong with the request params'
+}
+
+if response.status_code is response_codes.keys():
+    print(response_codes[response.status_code])
+
+else:
+    print('Unknown status code')
