2405b9f9d46311bab664c394c3f91469af86b3f2
George Perdrizet <george@perdrizet.org>
2025-02-10 19:40:28 +0000

    Finished exercise 3.

--- exercises/03-response-body/app.py
+++ exercises/03-response-body/app.py
@@ -1,3 +1,12 @@
 import requests
 
-url = "https://assets.breatheco.de/apis/fake/sample/random-status.php"
+url="https://assets.breatheco.de/apis/fake/sample/random-status.php"
+response=requests.get(url)
+
+print(response.status_code)
+
+if response.status_code == '200':
+    print(response.text)
+
+else:
+    print('Something went wrong')
